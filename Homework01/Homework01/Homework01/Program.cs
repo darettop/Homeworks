5e1f77a22277c83a9aef6f5e7325ee45f51181d4
Darko Petrov <daremc2123@gmail.com>
2020-03-03 14:52:06 +0100

    Homework01 added

---
+++ Homework01/Homework01/Homework01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traffic Light...");
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red light - STOP!");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow light - PREPARE.");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green light - GO!");
+            Console.ResetColor();
+            Thread.Sleep(1000);
+            Console.ReadLine();
+        }
+    }
+}
