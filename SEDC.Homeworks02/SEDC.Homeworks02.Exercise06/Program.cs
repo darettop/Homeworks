eff244b516d0fcb40ad2d8a76df551881939e995
Darko Petrov <daremc2123@gmail.com>
2020-03-06 22:15:37 +0100

    Homework 02

---
+++ SEDC.Homeworks02/SEDC.Homeworks02.Exercise06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SEDC.Homeworks02.Exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter the First Number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Second Number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2 ) {
+                Console.WriteLine("The first number is larger than the second number");
+                if (num1 % 2 == 0)
+                    Console.WriteLine("The first number is even");
+                else
+                    Console.WriteLine("The first number is odd");
+            }
+
+            if (num2 > num1)
+            {
+                Console.WriteLine("The second number is larger than the first number");
+                if (num2 % 2 == 0)
+                    Console.WriteLine("The second number is even");
+                else
+                    Console.WriteLine("The second number is odd");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SEDC.Homeworks02/SEDC.Homeworks02.Exercise07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SEDC.Homeworks02.Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a number between one and three: ");
+            number = int.Parse(Console.ReadLine());
+            switch(number)
+            {
+                case 1:
+                   Console.WriteLine("You got a new car!");
+                   break;
+                case 2:
+                    Console.WriteLine("You got a new plane!");
+                    break;
+                case 3:
+                    Console.WriteLine("You got a new bike!");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+               }
+            Console.ReadLine();
+        }
+    }
+}
